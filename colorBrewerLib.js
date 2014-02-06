2ba481fa74e9d13a9e802281427c2934a6155845
David Custodio <davidlacustodio@gmail.com>
2014-02-06 23:44:22 +0000

    added readme

--- colorBrewerLib.js
+++ colorBrewerLib.js
@@ -5,6 +5,8 @@ window.ColorBrewer = (function () {
 
     var colorBrewerLib = {};
 
+// This product includes color specifications and designs developed by Cynthia Brewer (http://colorbrewer.org/).
+// JavaScript specs as packaged in the D3 library (d3js.org). Please see license at http://colorbrewer.org/export/LICENSE.txt
     var colorbrewer = {YlGn: {
         3: ["#f7fcb9","#addd8e","#31a354"],
         4: ["#ffffcc","#c2e699","#78c679","#238443"],
